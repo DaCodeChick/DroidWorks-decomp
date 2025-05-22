8e0093aa66a2bacf4bbfde3caee2e800c0dea5f5
DaCodeChick <peepvtuber@gmail.com>
2025-05-22 15:02:12 -0700

    FindClosestColor

--- src/util.cpp
+++ src/util.cpp
@@ -1,5 +1,47 @@
 #include "util.h"
 
+// Win: 005020d0
+byte FindClosestColor(const byte *palette, uint count, float red, float green, float blue)
+{
+	const byte *pbVar1;
+	float fVar2;
+	uint uVar3;
+	byte bVar4;
+	const byte *pbVar6;
+	float local_4;
+	uint uVar5;
+
+	uVar5 = 0;
+	bVar4 = 0;
+	uVar3 = 0;
+	local_4 = 3.4e+38f;
+	if (count != 0)
+	{
+		do
+		{
+			if (local_4 <= 0.0f)
+			{
+				return (byte)uVar5;
+			}
+			bVar4 = *palette;
+			pbVar1 = palette + 1;
+			pbVar6 = palette + 2;
+			palette = palette + 3;
+			fVar2 = ((float)*pbVar6 - blue) * ((float)*pbVar6 - blue) * 0.114f +
+			        ((float)*pbVar1 - green) * ((float)*pbVar1 - green) * 0.587f +
+			        ((float)bVar4 - red) * ((float)bVar4 - red) * 0.299f;
+			if (fVar2 < local_4)
+			{
+				uVar5 = uVar3;
+				local_4 = fVar2;
+			}
+			bVar4 = (byte)uVar5;
+			uVar3 = uVar3 + 1;
+		} while (uVar3 < count);
+	}
+	return bVar4;
+}
+
 // Win: 00501ba0
 uint StringHash(const char *str, int divisor)
 {
--- src/util.h
+++ src/util.h
@@ -2,4 +2,5 @@
 
 #include "typedefs.h"
 
+byte FindClosestColor(const byte *palette, uint paletteSize, byte r, byte g, byte b);
 uint StringHash(const char *str, int divisor);
