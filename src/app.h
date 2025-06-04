1ac894bc1663ded0226b22450c2f3e5260e06ffa
DaCodeChick <peepvtuber@gmail.com>
2025-06-04 00:06:44 -0700

    file routines were Win32 API, whoops

--- src/app.h
+++ src/app.h
@@ -7,7 +7,31 @@ typedef HINSTANCE AppHandle;
 typedef HWND WindowHandle;
 #endif
 
+/**
+ * @brief Get the application handle.
+ * 
+ * @return The handle to the application.
+ */
 AppHandle GetAppHandle();
+
+/**
+ * @brief Get the application window handle.
+ * 
+ * @return The handle to the application window.
+ */
 WindowHandle GetAppWindow();
+
+/**
+ * @brief Set the application handle.
+ * 
+ * @param handle The handle to set for the application.
+ */
 void SetAppHandle(AppHandle handle);
+
+/**
+ * @brief Set the application window handle.
+ * 
+ * @param handle The handle to set for the application window.
+ * @return The window handle.
+ */
 WindowHandle SetAppWindow(WindowHandle handle);
--- src/file.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-int MakeDirectory(const char *path);
-int RemoveFile(const char *path);
-bool TryRemoveFile(const char *path);
